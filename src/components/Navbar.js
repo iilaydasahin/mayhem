9be578e6d01cfec7f3182330e33755b0f2ae2ef5
Ilayda Sahin <iilaydasahin@gmail.com>
2024-01-18 11:08:51 +0300

    changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,7 +15,7 @@ function Navbar({ handleSubscribe }) { // handleSubscribe prop'u ekle
   return (
     <div className="navbar">
       <div className="leftSide" id={openLinks ? "open" : "close"}>
-        <img src={Logo} />
+        <img src={Logo} alt=""/>
         <div className="hiddenLinks">
           <Link to="/"> Home </Link>
           <Link to="/features"> Features </Link>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import BannerVideo from "../assets/Mayhem.mp4";
 import "../styles/Home.css";
 
